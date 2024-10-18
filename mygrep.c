5889ae7880417776f0a2ac033ee19051f03523ab
mohamed28875701 <mohamedbenhadjlakhal@gmail.com>
2024-10-18 21:55:57 +0100

    multiple selection and colors

--- mygrep.c
+++ mygrep.c
@@ -3,6 +3,28 @@
 #include <unistd.h>
 #include <string.h>
 #include <regex.h>
+void print_finds(regmatch_t* pmatch,char* string,regex_t regex){
+    while(regexec(&regex,string,1,pmatch,0)==0){
+        if(pmatch[0].rm_so==-1){
+            continue;
+        }
+        size_t match_len=pmatch[0].rm_eo - pmatch[0].rm_so;
+        char* match = malloc(match_len+1);
+        strncpy(match,string+pmatch[0].rm_so,match_len);
+        match[match_len]='\0';
+        printf("\033[31m%s\033[0m",match);
+        for(int i=pmatch[0].rm_eo;i!=strlen(string);i++){
+            if(string[i]!='\n')
+                printf("%c",string[i]);
+            else{
+                printf("\n");
+                break;
+            }
+        }
+            string+=pmatch[0].rm_eo;
+        free(match);
+    }
+}
 void find_patterns(FILE* f,const char* pattern){
     regex_t regex;
     regmatch_t pmatch[1];
@@ -12,19 +34,10 @@ void find_patterns(FILE* f,const char* pattern){
     char* string =malloc(fsize+1);
     fread(string,fsize,1,f);
     string[fsize]='\0';
-    if(regcomp(&regex,pattern,0)==0){
-        while(regexec(&regex,string,1,pmatch,0)==0){
-                if(pmatch[0].rm_so==-1){
-                    continue;
-                }
-                size_t match_len=pmatch[0].rm_eo - pmatch[0].rm_so;
-                char* match = malloc(match_len+1);
-                strncpy(match,string+pmatch[0].rm_so,match_len);
-                match[match_len]='\0';
-                printf("%s\n",match);
-                string+=pmatch[0].rm_eo;
-                free(match);
-        }
+    if(regcomp(&regex,pattern,REG_EXTENDED)==0){
+        print_finds(pmatch,string,regex);
+    }else{
+        printf("Cant compile regex");
     }
 
 }
@@ -36,13 +49,13 @@ int main(int argc,char* argv[]){
     if(strcmp(argv[1],"-v")==0 ||strcmp(argv[1],"--version")==0){
         printf("version 1.0.0\n");
     }
-    find_patterns(fopen("mygrep.c","rb"),"print");
-    char* dir = malloc(100*sizeof( char));
+    find_patterns(fopen("mygrep.c","rb"),"[A-Z]{3,}");
+    /*char* dir = malloc(100*sizeof( char));
     if(getcwd(dir,100)!=NULL){
         printf("%s",dir);
     }else{
         printf("something went wrong");
     }
-    free(dir);
+    free(dir);*/
     return 1;
 }
